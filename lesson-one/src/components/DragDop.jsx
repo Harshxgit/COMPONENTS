14485abd337802e5305172edebd5c4517295cdaa
Harshxgit <htp.codes@gmail.com>
2024-10-30 22:21:44 +0530

    build drag and drop

--- lesson-one/src/components/DragDop.jsx
+++ lesson-one/src/components/DragDop.jsx
@@ -10,7 +10,7 @@ export default function DragDop() {
 
 const Board = () => {
   const [cards, setCards] = useState(DEFAULT_CARDS);
- 
+
   return (
     <div className="flex h-full w-full gap-3 overflow-scroll p-12">
       <Column
@@ -41,6 +41,7 @@ const Board = () => {
         cards={cards}
         setCards={setCards}
       />
+      <BurnBarrel setCards={setCards} />
     </div>
   );
 };
@@ -50,10 +51,92 @@ const Column = ({ title, column, headingColor, cards, setCards }) => {
   const [active, setActive] = useState(null);
   const filteredCards = cards.filter((c) => c.column === column);
 
-  const handleDragStart = (e,card)=>{
-        e.dataTransfer.setData("card",card.id)
-  }
+  const handleDragStart = (e, card) => {
+    e.dataTransfer.setData("card", card.id);
+  };
 
+  const handleDragEnd = (e) => {
+    const cardId = e.dataTransfer.getData("card");
+    setActive(false);
+    clearHighlights();
+
+    const indicators = getIndicators();
+    const { element } = getNearestIndicators(e, indicators);
+
+    const before = element.dataset.before || "-1";
+    if (before !== cardId) {
+      let copy = [...cards];
+
+      let cardToTransfer = copy.find((c) => c.id === cardId);
+      if (!cardToTransfer) return;
+
+      cardToTransfer = { ...cardToTransfer, column };
+
+      copy = copy.filter((c) => c.id !== cardId);
+
+      const moveToBack = before === "-1";
+
+      if (moveToBack) {
+        copy.push(cardToTransfer);
+      } else {
+        const insertAtIndex = copy.findIndex((el) => el.id === before);
+        if (insertAtIndex === undefined) return;
+        copy.splice(insertAtIndex, 0, cardToTransfer);
+      }
+      setCards(copy);
+    }
+  };
+
+  const clearHighlights = (els) => {
+    const indicators = els || getIndeicators();
+    indicators.forEach((i) => {
+      i.style.opacity = "0";
+    });
+  };
+
+  const highlighIndicator = (e) => {
+    const indicators = getIndicators();
+    clearHighlights(indicators);
+
+    const el = getNearestIndicators(e, indicators);
+
+    el.element.style.opacity = "1";
+  };
+
+  const getNearestIndicators = (e, indicators) => {
+    const DISTANCE_OFFSET = 50;
+    const el = indicators.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect();
+
+        const offset = e.clientY - (box.top + DISTANCE_OFFSET);
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      {
+        offset: Number.NEGATIVE_INFINITY,
+        element: indicators[indicators.length - 1],
+      }
+    );
+
+    return el;
+  };
+  const getIndicators = () => {
+    return Array.from(document.querySelectorAll(`[data-column="${column}"]`));
+  };
+  const handleDragLeave = () => {
+    clearHighlights();
+    setActive(false);
+  };
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    highlighIndicator(e);
+
+    setActive(true);
+  };
   return (
     <div className="w-56 shrink-0">
       <div className="mb-3 flex items-center justify-between">
@@ -63,15 +146,17 @@ const Column = ({ title, column, headingColor, cards, setCards }) => {
         </span>
       </div>
       <div
-        className={`h-full w-full transition-colors  ${
+        onDrop={handleDragEnd}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        className={`h-full w-full transition-colors ${
           active ? "bg-neutral-800/50" : "bg-neutral-800/0"
         }`}
       >
         {filteredCards.map((c) => {
           return <Card key={c.id} {...c} handleDragStart={handleDragStart} />;
-          
         })}
-        <DropIndicator beforeid={-1} column={column} />
+        <DropIndicator beforeid={null} column={column} />
         <AddCard column={column} setCards={setCards} />
       </div>
     </div>
@@ -79,16 +164,22 @@ const Column = ({ title, column, headingColor, cards, setCards }) => {
 };
 
 //Card Component
-const Card = ({ title, id, column ,handleDragStart}) => {
+const Card = ({ title, id, column, handleDragStart }) => {
   return (
     <>
       <DropIndicator beforeid={id} column={column} />
 
       <motion.div
         layout
-        layoutid={id}
+        layoutId={id}
         draggable="true"
-        onDragStart = {(e)=>handleDragStart()}
+        onDragStart={(e) =>
+          handleDragStart(e, {
+            title,
+            id,
+            column,
+          })
+        }
         className="cursor-grab rounded border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing"
       >
         <p className="text-sm text-neutral-100">{title}</p>
@@ -97,20 +188,47 @@ const Card = ({ title, id, column ,handleDragStart}) => {
   );
 };
 
-const DropIndicator = ({ beforeid, column }) => {
+const DropIndicator = ({ beforeId, column }) => {
   return (
     <div
-      data-before={beforeid || -1}
+      data-before={beforeId || "-1"}
       data-column={column}
-      className="h-0.5 w-full bg-green-600 opacity-0 "
+      className="my-0.5 h-0.5 w-full bg-violet-400 opacity-0"
     />
   );
 };
 
 const BurnBarrel = ({ setCards }) => {
   const [active, setActive] = useState(false);
+
+  const handleDragEnd = (e) => {
+    const id = e.dataTransfer.getData("card");
+    console.log(id);
+    setCards((pv) => pv.filter((c) => c.id !== id));
+    setActive(false);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setActive(true);
+  };
+
+  const handleDragLeave = (e) => {
+    setActive(false);
+  };
   return (
-    <div className="mt-10 grid h-56 w-56 shrink-0 place-content-center rounded border text-3xl" />
+    <div
+      onDrop={handleDragEnd}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      className={`mt-10 grid h-56 w-56 shrink-0 place-content-center rounded border text-3xl ${
+        active
+          ? "border-red-800 bg-red-800/20 text-red-500"
+          : "border-neutral-500 bg-neutral-500/20 text-neutral-500"
+      } `}
+    >
+      trash me here
+    </div>
   );
 };
 
